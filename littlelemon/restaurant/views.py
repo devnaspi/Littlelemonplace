9d10a5a7a8ec16dd7a7f803550acf8ccddc0dc02
naspi <iampelumifat@gmail.com>
2023-01-11 19:47:55 +0100

    Create Api End-point for Menu and table bookings

--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -1,5 +1,27 @@
 from django.shortcuts import render
+from rest_framework import generics, viewsets
+from rest_framework.response import Response
+from .serializers import menuSerializer, bookingSerializer
+
+from .models import Menu, Booking
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html', {})
+
+class menuItemView(generics.ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = menuSerializer
+
+
+class singleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = menuSerializer
+
+class BookingViewSet(viewsets.ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = bookingSerializer
+    
+
+
+    
